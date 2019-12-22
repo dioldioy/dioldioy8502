a0571320786018e917565f7307f7fa2f323a6fc9
dioldioy <57718730+dioldioy@users.noreply.github.com>
2019-12-22 14:00:03 +0800

    Add files via upload
    实验三

---
+++ ExerciseIOException.java
@@ -0,0 +1,7 @@
+package ExerciseTest;
+
+public class ExerciseIOException extends Exception{
+	private static final long serialVersionUID = 2854125389344801376L;
+	public ExerciseIOException() {}
+	public ExerciseIOException(String msg) {super(msg);}
+}
